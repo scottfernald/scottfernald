bec881abe433c876ed0791ccc48569e20f93f30b
SuperJSDEV <137197946+potentialdev-web@users.noreply.github.com>
2023-08-24 13:48:16 -0600

    Update github url

--- constans/common.ts
+++ constans/common.ts
@@ -1,5 +1,5 @@
 export const SOCIALS = {
-  GH: 'https://github.com/superJSDEV',
+  GH: 'https://github.com/potentialdev-web',
   IG: 'https://www.instagram.com/dedeard.js',
   LI: 'https://www.linkedin.com/in/arnold-choy/',
   WA: 'https://api.whatsapp.com/send?phone=6281343912883&text=%3CChatMe%20%2F%3E',
