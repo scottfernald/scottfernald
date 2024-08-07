a9750acca37c649680781804f99b773937883807
PotentialDev <potentialdev69@gmail.com>
2024-08-07 09:56:33 +0100

    update

--- constans/common.ts
+++ constans/common.ts
@@ -2,5 +2,5 @@ export const SOCIALS = {
   GH: 'https://github.com/techexpert0119',
   IG: 'https://www.instagram.com/dedeard.js',
   LI: 'https://www.linkedin.com/in/adrianchu0119/',
-  WA: 'https://api.whatsapp.com/send/?phone=%2B17708727817&text&type=phone_number&app_absent=0',
+  WA: 'https://api.whatsapp.com/send/?phone=%2B61485816753&text&type=phone_number&app_absent=0',
 }
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -57,7 +57,7 @@ const ContactPage = () => {
               >
                 <GitHubIcon />
               </a>
-              {/* <a
+              <a
                 href={SOCIALS.WA}
                 aria-label="WhatsApp"
                 target="_blank"
@@ -65,7 +65,7 @@ const ContactPage = () => {
                 className="mx-2 inline-block p-2 opacity-80 hover:opacity-95"
               >
                 <WhatsAppIcon />
-              </a> */}
+              </a>
             </div>
           </div>
         </div>
